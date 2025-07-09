98432da2ad59a1f5983db063a172d81ce18c5d4d
HugoPintoIEFP <hmphugopinto@gmail.com>
2025-07-09 23:11:19 +0100

    Create pycracker.py

---
+++ src/pycracker.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+    Tenta 'crackar' um ficheiro de palavras passe com uma estrutura identica ao 'etc/shadow'. Apresentam-se aqui duas formas
+    de ler parâmetros da linha de comandos:
+    1. Utilizando o móduloi argparse (presente na biblioteca padrão)
+    2.Utilizando o módulo  docopt (disponível no PyPI)
+
+    More to come.
+"""
+import os
+from docopt import docopt
+
+
+def main():
+    """
+        Pycracker entry point. Reads command line arguments and using the docopt library and calls the appropriate functions
+    """
+
+
+if __name__ == '__main__':
+    main()
