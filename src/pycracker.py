675555a17ca618e9e621367de0c97ed329f1858f
HugoPintoIEFP <hmphugopinto@gmail.com>
2025-07-09 23:43:59 +0100

    Update pycracker.py

--- src/pycracker.py
+++ src/pycracker.py
@@ -12,14 +12,29 @@ import os
 from docopt import docopt
 import sys
 
+DEFAULT_PWD_FILE = '/etc/shadow'
+
 def main():
     """
         Pycracker entry point. Reads command line arguments and using the docopt library and calls the appropriate functions
     """
     doc = f"""
-    Pycracker is a password cracker writee
+    Pycracker is a password cracker written in Python3. Using a password dictionary, 
+    it searches for user with passwords in that dictionary
 
+    Usage:
+        {sys.argv[0]} <dictionary> [<passwords>] [-u USER] [-v]
+
+    Options:
+        -h, --help              Show help
+        <passwords>             /etc/shadow-like file [default: {DEFAULT_PWD_FILE}]
+        <dictionary>            Password dictionary
+        -u USER, --user=USER    Search password for this USER only
+        -v, --verbose           Increase verbosity level
     """
+
+    args = docopt(doc)
+    print(args)
 
 if __name__ == '__main__':
     main()
